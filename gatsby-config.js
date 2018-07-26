cab596e2d2d2ccd158e846fb6cd4b2de34cdbbc7
Josef Rousek <josef.rousek@gmail.com>
2018-07-26 13:12:33 +0200

    Switch to tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    `gatsby-plugin-postcss`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
@@ -20,18 +20,28 @@ module.exports = {
       },
     },
     'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: `gatsby-transformer-remark`,
       options: {
-        plugins: [],
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+            },
+          },
+        ],
       },
     },
+    'gatsby-transformer-sharp',
     {
       resolve: 'gatsby-plugin-netlify-cms',
       options: {
         modulePath: `${__dirname}/src/cms/cms.js`,
-        stylesPath: `${__dirname}/src/components/all.sass`,
+        stylesPath: `${__dirname}/src/styles/index.css`,
       },
     },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
-import './all.sass'
+import '../styles/index.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
