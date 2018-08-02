1c68152395618a6abe9bff1abbf8b9e7530fef96
Josef Rousek <josef.rousek@gmail.com>
2018-08-02 08:17:17 +0200

    Fix css media query nesting

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,19 @@
+
+var plugins = [
+  require('postcss-import')(),
+  require(`tailwindcss`)(`./src/styles/tailwind.js`),
+  require("precss"),
+  require('postcss-preset-env')({
+    stage: 0
+  }),
+]
+
+if (process.env.NODE_ENV == 'production') {
+  plugins.push(require('cssnano')({
+    preset: 'default',
+  }))
+}
+
 module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("precss"),
-    require(`tailwindcss`)(`./src/styles/tailwind.js`),
-    require(`autoprefixer`)()
-  ]
+  plugins: plugins
 }
