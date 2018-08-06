3ea2c7e45b6f80356db223ce840558d8f7275c40
Josef Rousek <josef.rousek@gmail.com>
2018-08-06 09:16:48 +0200

    Footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Helmet from 'react-helmet'
 
 import Navbar from '../components/Navbar'
+import InstagramIcon from '../components/icons/Instagram'
+import FacebookIcon from '../components/icons/Facebook'
+import LinkedInIcon from '../components/icons/LinkedIn'
 import '../styles/index.css'
 
 const TemplateWrapper = ({ children }) => (
@@ -17,8 +20,8 @@ const TemplateWrapper = ({ children }) => (
     </Helmet>
     <Navbar />
     <div>{children}</div>
-    <footer className="relative mt-10 pt-10 bg-grey-darkest text-white text-center">
-      <h2 className="">Liduše píše</h2>
+    <footer className="relative mt-8 pt-8 bg-grey-darkest text-white text-center">
+      <h2 className="mb-8">Liduše píše</h2>
       <form
         name="subscription"
         method="post"
@@ -26,7 +29,7 @@ const TemplateWrapper = ({ children }) => (
         className="flex flex-col mb-3"
       >
         <input type="hidden" name="form-name" value="subscription" />
-        <div className="flex flex-col self-center my-5 text-sm">
+        <div className="flex flex-col self-center text-sm">
           <div className="sm:flex">
             <input
               className="appearance-none sm:w-full text-grey-darker border border-grey-dark py-2 px-4 mr-4 mb-4 sm:mb-0 leading-tight"
@@ -53,7 +56,29 @@ const TemplateWrapper = ({ children }) => (
           </label>
         </div>
       </form>
-      <p className="py-10 text-sm text-grey-light">© Lidmila Maršálková</p>
+      <div className="flex justify-between items-center py-5">
+        <p className="text-sm text-grey-light mx-10">© Lidmila Maršálková</p>
+        <div className="flex justify-end mx-10">
+          <a
+            className="inline-block mx-1"
+            href="https://www.instagram.com/lidmilka/"
+          >
+            <InstagramIcon className="w-8 fill-current text-white hover:text-grey" />
+          </a>
+          <a
+            className="inline-block mx-1"
+            href="https://www.facebook.com/copywriterlidmila"
+          >
+            <FacebookIcon className="w-8 fill-current text-white hover:text-grey" />
+          </a>
+          <a
+            className="inline-block mx-1"
+            href="https://www.linkedin.com/in/lmarsalkova/"
+          >
+            <LinkedInIcon className="w-8 fill-current text-white hover:text-grey" />
+          </a>
+        </div>
+      </div>
     </footer>
   </div>
 )
