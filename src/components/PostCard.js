fec53d0a39e26dcd3c69503a25ed5fc5a769ea89
Josef Rousek <josef.rousek@gmail.com>
2018-08-06 09:50:26 +0200

    Card should have fixed height on blog page

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,29 +1,33 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Img from 'gatsby-image'
+import _ from 'lodash'
 
 const PostCard = ({ post }) => (
   <Link
     to={post.fields.slug}
     className="block mx-auto my-4 max-w-lg w-full lg:flex no-underline"
   >
-    <div className="h-48 lg:h-auto lg:w-96 flex-none bg-cover bg-center text-center overflow-hidden">
+    <div className="h-48 lg:h-72 lg:w-96 flex-none bg-cover bg-center text-center overflow-hidden">
       <Img
         sizes={post.frontmatter.cover_image.childImageSharp.sizes}
         outerWrapperClassName="h-full"
-        className="h-full"
+        className="h-72 w-full gatsby-bg-image"
       />
     </div>
     <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white p-4 flex flex-col justify-between leading-normal">
       <div className="mb-8">
-        <p class="text-sm text-grey-dark flex items-center">
+        <p className="text-sm text-grey-dark flex items-center">
           {post.frontmatter.date}
         </p>
         <div className="text-black font-bold text-xl mb-2">
           {post.frontmatter.title}
         </div>
         <p className="text-grey-darker text-base">
-          {post.frontmatter.description}
+          {_.truncate(post.frontmatter.description, {
+            length: 335,
+            separator: ' ',
+          })}
         </p>
       </div>
     </div>
